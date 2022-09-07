5a770ccfd44b2f7ddff063d18020c166c477c3da
jeongbin <qls2467@gmail.com>
2022-09-07 11:33:07 -0600

    Middlewares and First Request

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
   console.log("Connected to MongoDB");
 });
 
+//Minddleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.send("welcome to homepage");
+});
+
 app.listen(8800, () => {
   console.log("Backend server is running");
 });
